5a1cfce9b73b074a0052527c827f87c03a6fdd18
Ivan <novvan2002@gmail.com>
2023-08-14 14:11:12 +0300

    get last levels gu changes

--- src/service/levelsGuService.ts
+++ src/service/levelsGuService.ts
@@ -1,11 +1,213 @@
 import { LevelGu } from "../entities/LevelGu"
 import { River } from "../entities/River"
 import { AppDataSource } from "../data-source"
-import { hydronodesData } from "./levelsGuData"
+// import { hydronodesData } from "./levelsGuData"
 import { Between } from 'typeorm';
 
 const LevelsGuRepository = AppDataSource.getRepository(LevelGu)
 const RiverRepository = AppDataSource.getRepository(River)
+
+let hydronodesData = [
+    {
+        id: 1,
+        hydronode: 'Судоходный г/у №1 Дубой',
+        river: 'Пина',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [52.079142, 25.774201]
+    },
+    {
+        id: 2,
+        hydronode: 'Судоходный г/у №2 Переруб',
+        river: 'ДБК',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [52.035181, 25.618088]
+    }, 
+    {
+        id: 3,
+        hydronode: 'Судоходный г/у №3 Рагодощ',
+        river: 'ДБК',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [52.023500, 25.472534]
+    },
+    {
+        id: 4,
+        hydronode: 'Судоходный г/у №4 Овзичи',
+        river: 'ДБК',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [52.022081, 25.328837]
+    },
+    {
+        id: 5,
+        hydronode: 'Судоходный г/у №5 Ляховичи',
+        river: 'ДБК',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [52.036990, 25.163390]
+    },
+    {
+        id: 6,
+        hydronode: 'Судоходный г/у Кобрин',
+        river: 'ДБК',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [52.212479, 24.399268]
+    },
+    {
+        id: 7,
+        hydronode: 'Судоходный г/у №8 Залузье',
+        river: 'Мухавец',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [52.191919, 24.128263]
+    },
+    {
+        id: 8,
+        hydronode: 'Судоходный г/у №9 Новосады',
+        river: 'Мухавец',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [52.141047, 23.953104]
+    },
+    {
+        id: 9,
+        hydronode: 'Судоходный г/у №10 Тришин',
+        river: 'Мухавец',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [52.088838, 23.743320]
+    },
+    {
+        id: 10,
+        hydronode: 'Судоходный г/у №11 Качановичи',
+        river: 'Припять',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [52.116139, 26.429919]
+    },
+    {
+        id: 11,
+        hydronode: 'Судоходный г/у №12 Стахово',
+        river: 'Припять',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [52.114592, 26.736549]
+    },
+    {
+        id: 12,
+        hydronode: 'Брест плотина',
+        river: 'Мухавец',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [52.083828006110416, 23.663883865425976]
+    },
+    {
+        id: 13,
+        hydronode: 'Судоходный шлюз Витебская ГЭС',
+        river: 'Западная Двина',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [55.249773, 30.160869]
+    },
+    {
+        id: 14,
+        hydronode: 'Гродненская ГЭС',
+        river: 'Неман',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [53.645261, 23.973409]
+    },
+    {
+        id: 15,
+        hydronode: 'Белорусская часть г/у Кужинец',
+        river: 'Августовский канал',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [53.860340, 23.528939]
+    },
+    {
+        id: 16,
+        hydronode: 'Судоходный г/у Волкушек',
+        river: 'Августовский канал',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [53.859118, 23.549525]
+    },
+    {
+        id: 17,
+        hydronode: 'Судоходный г/у Домбровка',
+        river: 'Августовский канал',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [53.862782, 23.624258]
+    },
+    {
+        id: 18,
+        hydronode: 'Судоходный г/у Немново',
+        river: 'Августовский канал',
+        date: '—',
+        level1: '—',
+        level2: '—',
+        level1Change: '—',
+        level2Change: '—',
+        coords: [53.861766, 23.748762]
+    },
+
+]
 
 export const getAll = async () => {
     let levels = await LevelsGuRepository.find({
@@ -52,7 +254,7 @@ export const getAll = async () => {
 
 export const getLastLevels = async () => {
 
-    let rows = [...hydronodesData];
+    let rows = [];
 
     let levels = await LevelsGuRepository.find(
         {
@@ -72,18 +274,16 @@ export const getLastLevels = async () => {
         )
     })
 
-    rows = rows.map((row) => {
+    rows = hydronodesData.map((row) => {
         let rowData = levelsDto.filter((dat) => (dat.hydronode === row.hydronode));
         if (rowData.length === 0) return row;
         let lastRecord = rowData[0];
         rowData.forEach((dat) => { if (dat.date.getTime() > lastRecord.date.getTime()) lastRecord = dat; })
-        row.level1 = lastRecord.level1;
-        row.level2 = lastRecord.level2;
-        row.level1Change = lastRecord.level1Change;
-        row.level2Change = lastRecord.level2Change;
-        row.date = lastRecord.date.toLocaleString().slice(0, 10);
-        row.river = lastRecord.river;
-        return row;
+      
+        return {
+            ...lastRecord,
+            date: lastRecord.date.toLocaleString().slice(0, 10)
+        }
       })
 
     return rows;
